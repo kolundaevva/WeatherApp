c50528b8e214b7e7e18d0ea106eaf8fea2701c9a
4tknox <kolundaevva@gmail.com>
2022-04-01 23:17:00 +0300

    Stroryboard settings

--- WeatherApp/View Controllers/ViewController.swift
+++ WeatherApp/View Controllers/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherIconImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+    }
+    
 }
 
