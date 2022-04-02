379a3b47dcdaee6cac5cf87c8e4e75138e50c3bc
4tknox <kolundaevva@gmail.com>
2022-04-02 21:43:17 +0300

    Add update interface func

--- WeatherApp/Models/NetworkWeatherManager.swift
+++ WeatherApp/Models/NetworkWeatherManager.swift
@@ -8,25 +8,32 @@
 import Foundation
 
 struct NetworkWeatherManager {
+    var onComlition: ((CurrentWeather) -> Void)?
+    
     func fetchCurrentWeather(forCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                self.parseJSON(with: data)
+                if let currentWeather = self.parseJSON(with: data) {
+                    self.onComlition?(currentWeather)
+                }
             }
         }
         task.resume()
     }
     
-    func parseJSON(with data: Data) {
+    func parseJSON(with data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         
         do {
             let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
+            guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else { return nil }
+            return currentWeather
         } catch let error as NSError {
             print(error.localizedDescription)
         }
+        return nil
     }
 }
--- WeatherApp/View Controllers/ViewController.swift
+++ WeatherApp/View Controllers/ViewController.swift
@@ -14,10 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
-    let networkWeatherManager = NetworkWeatherManager()
+    var networkWeatherManager = NetworkWeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        networkWeatherManager.onComlition = { currentWeather in
+            self.updateInterface(by: currentWeather)
+        }
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
     }
 
     @IBAction func searchPressed(_ sender: UIButton) {
@@ -26,5 +30,13 @@ class ViewController: UIViewController {
         }
     }
     
+    func updateInterface(by weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImage.image = UIImage(systemName: weather.systemItemNameString)
+        }
+    }
 }
 
